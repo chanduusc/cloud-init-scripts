e1244b47053a373f6ae5370b7702c7331eebedbe
Sandeep Chandu <schandu@paloaltonetworks.com>
2019-06-22 10:29:18 -0700

    Minor changes

--- ubuntu_aws.sh
+++ ubuntu_aws.sh
@@ -11,10 +11,12 @@ cd wrk2
 sudo make
 sudo /bin/cp wrk /bin/
 cd ..
-su - ubuntu -c "wget https://gist.githubusercontent.com/chanduusc/456068338371cc870ac72939e18c8300/raw/b4de3d6db0e6fef9818c15af8e8f1d5d73de0372/npm.sh"
+su - ubuntu -c "wget https://raw.githubusercontent.com/chanduusc/cloud-init-scripts/master/npm.sh"
 su - ubuntu -c "chmod a+x npm.sh"
 su - ubuntu -c "./npm.sh"
-su - ubuntu -c "cd /home/ubuntu/stressgrid/coordinator/;yes |MIX_ENV=prod mix deps.get;yes |MIX_ENV=prod mix release"
-su - ubuntu -c "cd /home/ubuntu/stressgrid/generator/;yes |MIX_ENV=prod mix deps.get;yes |MIX_ENV=prod mix release"
+su - ubuntu -c "cd /home/ubuntu/stressgrid/coordinator/;yes |MIX_ENV=prod mix deps.get"
+su - ubuntu -c "cd /home/ubuntu/stressgrid/coordinator/;yes |MIX_ENV=prod mix release"
+su - ubuntu -c "cd /home/ubuntu/stressgrid/generator/;yes |MIX_ENV=prod mix deps.get"
+su - ubuntu -c "cd /home/ubuntu/stressgrid/generator/;yes |MIX_ENV=prod mix release"
 su - ubuntu -c "/home/ubuntu/stressgrid/coordinator/_build/prod/rel/coordinator/bin/coordinator start"
 su - ubuntu -c "/home/ubuntu/stressgrid/generator/_build/prod/rel/generator/bin/generator start"
--- ubuntu_azure.sh
+++ ubuntu_azure.sh
@@ -11,10 +11,12 @@ cd wrk2
 sudo make
 sudo /bin/cp wrk /bin/
 cd ..
-su - panadmin -c "wget https://gist.githubusercontent.com/chanduusc/456068338371cc870ac72939e18c8300/raw/b4de3d6db0e6fef9818c15af8e8f1d5d73de0372/npm.sh"
+su - panadmin -c "wget https://raw.githubusercontent.com/chanduusc/cloud-init-scripts/master/npm.sh"
 su - panadmin -c "chmod a+x npm.sh"
 su - panadmin -c "./npm.sh"
-su - panadmin -c "cd /home/panadmin/stressgrid/coordinator/;yes |MIX_ENV=prod mix deps.get;yes |MIX_ENV=prod mix release"
-su - panadmin -c "cd /home/panadmin/stressgrid/generator/;yes |MIX_ENV=prod mix deps.get;yes |MIX_ENV=prod mix release"
+su - panadmin -c "cd /home/panadmin/stressgrid/coordinator/;yes |MIX_ENV=prod mix deps.get"
+su - panadmin -c "cd /home/panadmin/stressgrid/coordinator/;yes |MIX_ENV=prod mix release"
+su - panadmin -c "cd /home/panadmin/stressgrid/generator/;yes |MIX_ENV=prod mix deps.get"
+su - panadmin -c "cd /home/panadmin/stressgrid/coordinator/;yes |MIX_ENV=prod mix release"
 su - panadmin -c "/home/panadmin/stressgrid/coordinator/_build/prod/rel/coordinator/bin/coordinator start"
 su - panadmin -c "/home/panadmin/stressgrid/generator/_build/prod/rel/generator/bin/generator start"
