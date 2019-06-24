74e3b4caeb7b8294925e1662230bdd74973b71ce
Sandeep Chandu <schandu@paloaltonetworks.com>
2019-06-24 12:48:39 -0700

    lighty and server files

--- ubuntu_aws.sh
+++ ubuntu_aws.sh
@@ -1,11 +1,18 @@
 #! /bin/bash
 sudo apt-get update
-sudo apt -y remove erlang-base-hipe erlang-crypto erlang-syntax-tools
+sudo apt -y remove erlang-base-hipe erlang-crypto erlang-syntax-tools nginx apache2
 wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb 
 sudo dpkg -i erlang-solutions_1.0_all.deb
 sudo apt-get update
 sudo apt-get -y install esl-erlang
-sudo apt -y install zlib1g zlib1g-dev elixir build-essential curl git libssl-dev libz-dev unzip
+sudo apt -y install zlib1g zlib1g-dev elixir build-essential curl git libssl-dev libz-dev unzip lighttpd
+rm -rf /etc/lighttpd/lighttpd.conf
+wget -O /etc/lighttpd/lighttpd.conf https://raw.githubusercontent.com/chanduusc/cloud-init-scripts/master/lighttpd.conf
+wget -O /etc/ssl/certs/rithvik.pem https://raw.githubusercontent.com/chanduusc/cloud-init-scripts/master/rithvik.pem
+sudo systemctl stop lighthttpd
+sudo systemctl reload lighthttpd
+sudo systemctl start lighthttpd
+
 sudo git clone https://github.com/giltene/wrk2.git
 cd wrk2
 sudo make
